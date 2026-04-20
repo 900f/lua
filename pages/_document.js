3644fa678df8bcb9fe995fabbc0552f15fbc1549
900f <logant210@outlook.com>
2026-04-20 21:48:29 +0100

    bruh

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Luvenn | Script Protection Platform</title>
+        <meta name="description" content="Host, protect and distribute your Roblox Lua scripts" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
