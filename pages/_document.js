f6d63dedbe58bd7dbe7d4c2cda455b95ac429550
900f <logant210@outlook.com>
2026-04-21 16:33:13 +0100

    Added deletions

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,10 @@ export default function Document() {
       <Head>
         <title>Luvenn | Script Protection Platform</title>
         <meta name="description" content="Host, protect and distribute your Roblox Lua scripts" />
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <meta name="theme-color" content="#7c3aed" />
       </Head>
       <body>
         <Main />
